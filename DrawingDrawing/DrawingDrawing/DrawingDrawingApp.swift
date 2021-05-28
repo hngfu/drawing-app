d2643c7e8c17392f30f4c5deec859c7438e9184a
hngfu <djsl1018@naver.com>
2021-05-28 16:17:14 +0900

    Init project

---
+++ DrawingDrawing/DrawingDrawing/DrawingDrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingDrawingApp.swift
+//  DrawingDrawing
+//
+//  Created by 조재흥 on 2021/05/30.
+//
+
+import SwiftUI
+
+@main
+struct DrawingDrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
