65b40e905d9318f899d955c262962221c6aef5d1
hngfu <djsl1018@naver.com>
2021-05-30 14:07:22 +0900

    Use DrawingScene instead of ContentView

--- DrawingDrawing/DrawingDrawing/DrawingDrawingApp.swift
+++ DrawingDrawing/DrawingDrawing/DrawingDrawingApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DrawingDrawingApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DrawingScene()
         }
     }
 }
---
+++ DrawingDrawing/DrawingDrawing/DrawingScene.swift
@@ -0,0 +1,22 @@
+//
+//  DrawingScene.swift
+//  DrawingDrawing
+//
+//  Created by 조재흥 on 2021/05/30.
+//
+
+import SwiftUI
+
+struct DrawingScene: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+        }
+    }
+}
+
+struct DrawingScene_Previews: PreviewProvider {
+    static var previews: some View {
+        DrawingScene()
+    }
+}
